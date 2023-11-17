33f4449df471fa32355240f2573fe8c2240132b8
soybean15 <marlonpadilla1593@gmail.com>
2023-11-17 17:00:08 +0800

    deploy

---
+++ js/about.ac215fea.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkgroup3"]=self["webpackChunkgroup3"]||[]).push([[443],{8719:function(n,u,r){r.r(u),r.d(u,{default:function(){return l}});var t=r(9269);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),s=[e];function c(n,u){return(0,t.wg)(),(0,t.iD)("div",a,s)}var o=r(7617);const i={},f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.ac215fea.js.map
